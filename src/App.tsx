47e4a7d95f2cbb5dfe2f741af9331c56eadc1002
Ben Thomas <thomas22@terpmail.umd.edu>
2025-10-10 18:19:19 -0400

    Simple Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { supabase } from './supabaseClient'
 import './App.css'
 
 function App() {
 
   const [count, setCount] = useState(0);
+
+  useEffect(()=>{
+    const fetchCount = async () => {
+      const { data } = await supabase
+        .from('Simple Counter')
+        .select('count')
+        .eq('id', 1)
+        .single()
+      if (data) setCount(data.count)
+    }
+    fetchCount()
+  }, [])
+
+  const increment = async () => {
+    const newCount = count + 1
+    setCount(newCount)
+
+    // Update the counter in the database
+    const {} = await supabase
+      .from('Simple Counter')
+      .update({ count: newCount })
+      .eq('id', 1)
+  }
 
   return (
     <>
@@ -12,14 +36,10 @@ function App() {
       </h1>
 
       <button
-        onClick={()=>{setCount(count + 1)}}
+        onClick={increment}
       >
-        Click me
+        Global Counter: {count}
       </button>
-
-      <p>
-        {count}
-      </p>
 
 
     </>

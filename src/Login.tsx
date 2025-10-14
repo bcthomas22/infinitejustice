4f26de988c8c6b276e05b8d8673d0a78e87a6c71
Ben Thomas <thomas22@terpmail.umd.edu>
2025-10-14 16:34:33 -0400

    Patch

--- src/Login.tsx
+++ src/Login.tsx
@@ -34,7 +34,7 @@ export function Login() {
         await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: window.location.origin,
+                redirectTo: "https://bcthomas22.github.io/infinitejustice/",
             },
         });
     }

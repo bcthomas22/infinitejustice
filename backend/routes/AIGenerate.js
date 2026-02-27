5b8b03ae74371d9e4ddf6fda12adbee22a45646f
Ben Thomas <thomas22@terpmail.umd.edu>
2026-02-27 18:39:25 -0500

    AI Prompting Init

--- backend/routes/AIGenerate.js
+++ backend/routes/AIGenerate.js
@@ -62,15 +62,7 @@ router.post("/compareTopics", async (req, res) => {
             input: [
                 {
                     role: "system",
-                    content: "You will be given two topics relating to ethical justice, positive or negative" +
-                    " you must rate, on a scale from 1 to 10 (No decimals) how well these topics corelate." +
-                    " The first topic must \"Lead to\" the second topic" +
-                    " if the second topic makes no sence compared to the first, or is profane, sexual, offensive, out of pocket, non-ethical, rate it low (less than 3)" +
-                    " if the second topic makes a drastic jump from something good to bad, or bad to good, these topics might not be related, so rank it mid to low (less than 7)" +
-                    " if the second topic genuinely makes sence, and actually comes from the first topic, and is ethical, rate it highly (above 6)" 
-                    
-                    + "Finally, provide a topic in prefferably one word that the second topic LEADS to, " + 
-                    "but also try to respond with a quality topic you would rate highly"
+                    content: process.env.INFINITE_JUSTICE_PROMPT
                 },
                 {
                     role: "user",
@@ -86,11 +78,11 @@ router.post("/compareTopics", async (req, res) => {
                         properties: {
                             score: { 
                                 type: "number",
-                                description: "Enter your score here for does topic 1 lead to topic 2"
+                                description: process.env.INFINITE_JUSTICE_SCORE_PROMPT
                             },
                             response: { 
                                 type: "string",
-                                description: "Enter your new topic, what does topic 1 lead to?"
+                                description: process.env.INFINITE_JUSTICE_RETURN_PROMPT
                             }
                         },
                         required: ["score", "response"],

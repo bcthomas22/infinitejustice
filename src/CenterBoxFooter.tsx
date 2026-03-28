fdb4eb884281fc43c7889cf684cfb2d483f62bf8
Ben Thomas <thomas22@terpmail.umd.edu>
2026-03-28 19:26:38 -0400

    Hints added

--- src/CenterBoxFooter.tsx
+++ src/CenterBoxFooter.tsx
@@ -1,25 +1,63 @@
 import { Send } from "lucide-react";
 import { Lightbulb } from "lucide-react";
-import { useState } from "react";
+import { Keyboard } from 'lucide-react';
+import { useEffect, useState } from "react";
 
 type CenterBoxFooterProps = {
     sendTopicToChat(s: string): void
     currentTopic: string
 }
 
+const API_BASE = import.meta.env.VITE_API_URL;
+const MAX_HINTS = 3;
+
 export function CenterBoxFooter(props: CenterBoxFooterProps) {
 
     const [inputText, setInputText] = useState<string>("");
+    const [showHints, setShowHints] = useState<boolean>(false);
+    const [hints, setHints] = useState<string[]>(["Hint 1", "Hint 2", "Hint 3"]);
 
     const send = () => {
         props.sendTopicToChat(inputText);
+        setHints([]);
         setInputText("")
     }
 
+    const getHints = async (topic: string) => {
+        if(topic === ""){
+            return;
+        }
+
+        const resHinArr = await fetch(`${API_BASE}/api/aiGenerate/getHintsFromTopic`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({topic: topic})
+        })
+
+        if(!resHinArr.ok){
+            throw new Error("Error with getting hints with ai");
+        }
+
+        const dataArr = await resHinArr.json()
+
+        setHints(dataArr.hints);
+    }
+
+    useEffect(() => {
+        if (showHints && hints.length === 0) {
+            getHints(props.currentTopic)
+        }
+        console.log(hints)
+    }, [showHints])
+
     return (
         <div className="center-box-footer">
-            <div className="chat-input">
-                <button className="icon-button hint-button">
+            {!showHints ? (
+                <div className="chat-input">
+                <button 
+                    className="icon-button hint-button"
+                    onClick={() => {setShowHints(true)}}
+                >
                     <Lightbulb />
                 </button>
                 <input 
@@ -31,11 +69,37 @@ export function CenterBoxFooter(props: CenterBoxFooterProps) {
                 ></input>
                 <button 
                     className="icon-button"
-                    onClick={send}>
-                    
+                    onClick={send}
+                >    
                     <Send />
                 </button>
             </div>
+            ) : (<>
+                <button
+                    className="icon-button hint-button"
+                    onClick={() => {setShowHints(false)}}
+                ><Keyboard /></button>
+
+                {hints.length === 0 && <div className="loading"></div>}
+
+                {hints.map((h, i) => {
+                    if (i >= MAX_HINTS) return;
+                    return (<button
+                        key={i}
+                        onClick={() => {
+                            setHints([]);
+                            setShowHints(false);
+                            setInputText("");
+                            props.sendTopicToChat(h)
+                        }}
+                    >
+                        {h}
+                    </button>)
+                })}
+            </>)
+            }
+
+            
         </div>
     )
 }
